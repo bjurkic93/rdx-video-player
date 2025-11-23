a533b38393819ad887b9ad5af8975a4c4f311f0d
bjurkic93 <111120599+bjurkic93@users.noreply.github.com>
2025-11-23 12:02:00 +0100

    Enable looping video playback

--- app/src/main/java/hr/reddiax/videoplayer/video/VideoPlayerScreen.kt
+++ app/src/main/java/hr/reddiax/videoplayer/video/VideoPlayerScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.tv.material3.ExperimentalTvMaterial3Api
@@ -29,6 +30,7 @@ fun VideoPlayerScreen(
         ExoPlayer.Builder(context).build().apply {
             val mediaItem = MediaItem.fromUri(videoUrl)
             setMediaItem(mediaItem)
+            repeatMode = Player.REPEAT_MODE_ALL
             prepare()
             playWhenReady = true
         }
