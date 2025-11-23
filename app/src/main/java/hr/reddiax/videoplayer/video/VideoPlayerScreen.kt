62680c97359945477bc7e2eed1d29b3ea506ad71
bjurkic93 <111120599+bjurkic93@users.noreply.github.com>
2025-11-23 11:53:40 +0100

    Improve player controls and loop playback

--- app/src/main/java/hr/reddiax/videoplayer/video/VideoPlayerScreen.kt
+++ app/src/main/java/hr/reddiax/videoplayer/video/VideoPlayerScreen.kt
@@ -21,8 +21,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
+import androidx.media3.ui.StyledPlayerView
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
 import hr.reddiax.videoplayer.ui.theme.RdXVideoPlayerTheme
@@ -34,11 +35,12 @@ fun VideoPlayerScreen(
 ) {
     val context = LocalContext.current
     val focusRequester = remember { FocusRequester() }
-    val playerViewState = remember { mutableStateOf<PlayerView?>(null) }
+    val playerViewState = remember { mutableStateOf<StyledPlayerView?>(null) }
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
             val mediaItem = MediaItem.fromUri(videoUrl)
             setMediaItem(mediaItem)
+            repeatMode = Player.REPEAT_MODE_ALL
             prepare()
             playWhenReady = true
         }
@@ -61,13 +63,15 @@ fun VideoPlayerScreen(
         Box(modifier = Modifier.fillMaxSize()) {
             AndroidView(
                 factory = { playerContext ->
-                    PlayerView(playerContext).apply {
+                    StyledPlayerView(playerContext).apply {
                         layoutParams =
                             android.view.ViewGroup.LayoutParams(
                             android.view.ViewGroup.LayoutParams.MATCH_PARENT,
                             android.view.ViewGroup.LayoutParams.MATCH_PARENT
                         )
                         useController = true
+                        controllerAutoShow = true
+                        controllerShowTimeoutMs = 3000
                         isFocusable = true
                         isFocusableInTouchMode = true
                         requestFocus()
